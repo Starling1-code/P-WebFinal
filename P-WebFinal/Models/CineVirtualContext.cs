7c1a29a690b3e1385da5d4af9c3732df80920817
Starling1-code <chichofranco252@gmail.com>
2022-08-10 14:11:56 -0400

    Add project files.

---
+++ P-WebFinal/Models/CineVirtualContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace P_WebFinal.Models
+{
+    public partial class CineVirtualContext : DbContext
+    {
+        public CineVirtualContext()
+        {
+        }
+
+        public CineVirtualContext(DbContextOptions<CineVirtualContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Pelicula> Peliculas { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("server=DESKTOP-5DH6KOH; database=CineVirtual; integrated security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pelicula>(entity =>
+            {
+                entity.HasKey(e => e.IdP)
+                    .HasName("PK__Pelicula__B770B53EECA1A98C");
+
+                entity.Property(e => e.IdP).HasColumnName("Id_P");
+
+                entity.Property(e => e.AñoP)
+                    .HasColumnType("datetime")
+                    .HasColumnName("Año_P");
+
+                entity.Property(e => e.DescripcionP)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("Descripcion_P");
+
+                entity.Property(e => e.DirectorP)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("Director_P");
+
+                entity.Property(e => e.GeneroP)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("Genero_P");
+
+                entity.Property(e => e.TituloP)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("Titulo_P");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ P-WebFinal/Models/Pelicula.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P_WebFinal.Models
+{
+    public partial class Pelicula
+    {
+        public int IdP { get; set; }
+        public string? TituloP { get; set; }
+        public string? GeneroP { get; set; }
+        public DateTime? AñoP { get; set; }
+        public string? DirectorP { get; set; }
+        public string? DescripcionP { get; set; }
+    }
+}
